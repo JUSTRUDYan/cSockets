2a21e1dc5c916db70e878da41209b3858cb7132b
RUDYan <112593269+JUSTRUDYan@users.noreply.github.com>
2024-04-27 18:45:01 +0300

    commit

--- webserver/main.c
+++ webserver/main.c
@@ -1,9 +1,61 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <cstdlib>
 #include <cstdio>
-#include <cstring> 
+#include <cstring>
+#include <unistd.h> // For close()
+#include <pthread.h>
+#include <vector>
+#include <iostream>
+
 #define PORT 8080  
+
+struct ClientInfo {
+    int socket;
+    struct sockaddr_in address;
+};
+
+std::vector<ClientInfo> clients;
+
+void *responseMessage(void *arg) {
+    int client_socket = *((int *)arg);
+    const char *message = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>\n";
+    send(client_socket, message, strlen(message), 0);
+    close(client_socket);
+    return NULL;
+}
+
+void *handleConnection(void *arg) {
+    int server_fd = *((int *)arg);
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+    
+    while (1) {
+        int client_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+        if (client_socket < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        ClientInfo new_client;
+        new_client.socket = client_socket;
+        new_client.address = address;
+        printf("New client connected from %s:%d\n", inet_ntoa(new_client.address.sin_addr), ntohs(new_client.address.sin_port));
+        clients.push_back(new_client);
+
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, responseMessage, (void *)&client_socket) != 0) {
+            perror("pthread_create failed");
+            close(client_socket);
+            continue;
+        }
+
+        pthread_detach(tid);
+    }
+
+    return NULL;
+}
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,14 +70,26 @@ int main() {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    bind(server_fd, address, address.sin_len);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
     if (listen(server_fd, 3) < 0) { 
-        perror("In listen"); 
+        perror("listen failed"); 
         exit(EXIT_FAILURE); 
     }
-    if (accept(server_fd, address, address.sin_len) < 0) {
-        perror("In accept");            
-        exit(EXIT_FAILURE);        
+
+    pthread_t tid;
+
+    if (pthread_create(&tid, NULL, handleConnection, (void *)&server_fd) != 0) {
+        perror("pthread_create failed");
+        exit(EXIT_FAILURE);
     }
-        return 0; // Added for main to return an integer
+
+    pthread_join(tid, NULL);
+
+    close(server_fd);
+    return 0; 
 }
